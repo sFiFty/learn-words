3f86ce84fff291510c0f4892b6c403e010c90c51
sFiFty <alexandr.rudin.my@gmail.com>
2018-06-17 17:54:17 +0300

    working with google cloud

--- common/models/words.js
+++ common/models/words.js
@@ -2,18 +2,27 @@
 
 module.exports = function(Words) {
   Words.translate = function(cb) {
-    var currentDate = new Date();
-    var currentHour = currentDate.getHours();
-    var OPEN_HOUR = 6;
-    var CLOSE_HOUR = 20;
-    console.log('Current hour is %d', currentHour);
-    var response;
-    if (currentHour >= OPEN_HOUR && currentHour < CLOSE_HOUR) {
-      response = 'We are open for business.';
-    } else {
-      response = 'Sorry, we are closed. Open daily from 6am to 8pm.';
-    }
-    cb(null, response);
+    const Translate = require('@google-cloud/translate');
+    const translate = new Translate();
+    const text = 'The text to translate, e.g. Hello, world!';
+    const target = 'The target language, e.g. ru';
+    console.log(target)
+    cb(null, target);
+    translate
+      .translate(text, target)
+      .then(results => {
+        let translations = results[0];
+        translations = Array.isArray(translations)
+          ? translations
+          : [translations];
+        console.log('Translations:');
+        translations.forEach((translation, i) => {
+          console.log(`${text[i]} => (${target}) ${translation}`);
+        });
+      })
+      .catch(err => {
+        console.error('ERROR:', err);
+      });
   }
   Words.remoteMethod(
     'translate', {
